fb85add778b5698271abc7db47354dbeefd641eb
Damiano Oldoni <damiano.oldoni@inbo.be>
2022-11-01 15:38:18 +0100

    Push work before stopping for holidays

--- sql/dwc_occurrence.sql
+++ sql/dwc_occurrence.sql
@@ -3,22 +3,12 @@ Created by Damiano Oldoni (INBO)
 */
 
 SELECT
-  o."locatie" || ':' || o."datum"       AS eventID,
--- RECORD-LEVEL
+  o."_record_id"                        AS eventID,
+  -- RECORD-LEVEL
   'HumanObservation'                    AS basisOfRecord,
 -- OCCURRENCE
-  o."locatie" || ':' || o."datum" || ':' || o."species_name_hash" AS occurrenceID,
-  o."opmerking"                         AS occurrenceRemarks,
-  CASE
-    WHEN o."n" > 0  THEN o."n"
-    ELSE NULL
-  END                                   AS individualCount,
-  CASE
-    WHEN o."n" = 0  THEN "absent"
-    ELSE "present"
-  END                                   AS occurrenceStatus,
--- TAXON
-  o."species"                           AS scientificName,
-  'Animalia'                            AS kingdom
-  FROM occurrences AS o
-  WHERE  o."code" <= 1
+  o."_record_id"                        AS occurrenceID,
+  o."scientificName"                    AS scientificName,
+  o."vernacularName"                    AS vernacularName
+  'Plantae'                             AS kingdom,
+  'species'                             AS taxonRank
